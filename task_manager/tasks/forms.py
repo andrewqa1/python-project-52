e22ec4d6bfde72a679ac02983a65d37b63dfc619
GLADCHENKO Andrey O <Andrey.GLADCHENKO@raiffeisen.ru>
2024-07-01 18:14:08 +0400

    filters added

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,9 +1,15 @@
-from django.forms import ModelForm
+import django.forms as forms
 
+from task_manager.labels.models import Label
 from task_manager.tasks.models import Task
 
 
-class TaskCreateUpdateForm(ModelForm):
+class TaskCreateUpdateForm(forms.ModelForm):
+    labels = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False,
+    )
 
     class Meta:
         model = Task
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,15 +1,12 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    ListView,
-    UpdateView,
-)
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
+from django_filters.views import FilterView
 
 from task_manager.mixins import AuthRequiredMixin, NoPermissionRedirectMixin
+from task_manager.tasks.filters import TaskFilter
 from task_manager.tasks.forms import TaskCreateUpdateForm
 from task_manager.tasks.mixins import CheckIsUserTaskCreatorMixin
 from task_manager.tasks.models import Task
@@ -28,9 +25,10 @@ class TaskCreateView(
         return super().form_valid(form)
 
 
-class TaskListView(NoPermissionRedirectMixin, AuthRequiredMixin, ListView):
+class TaskListView(NoPermissionRedirectMixin, AuthRequiredMixin, FilterView):
     model = Task
     template_name = "tasks/index.html"
+    filterset_class = TaskFilter
 
 
 class TaskUpdateView(
